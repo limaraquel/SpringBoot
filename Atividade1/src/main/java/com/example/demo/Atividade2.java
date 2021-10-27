c833a79f3cde4221f0eefeffdd60d6f8effd94c8
limaraquel <limarakel9@gmail.com>
2021-10-27 19:53:54 -0300

    Adicionando ativadade 1 e 2 de spring boot

---
+++ Atividade1/src/main/java/com/example/demo/Atividade2.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class Atividade2 {
+	@SpringBootApplication
+	public class Atividade1Application {
+
+		public static void main(String[] args) {
+			SpringApplication.run(Atividade1Application.class, args);
+		}
+}
+}
